956a947eb1c285c1e11c082596a29cd1be2c87cd
Juho Sulkonen <juho.sulkonen@gmail.com>
2025-02-13 00:36:12 +0200

    find_substr() korjaus

--- search.cpp
+++ search.cpp
@@ -1,5 +1,7 @@
 #include "search.h"
 
-int find_substr(const wstring& haystack, const wstring& needle) {
-    return haystack.find(needle);
+int find_substr(const wstring &haystack, const wstring &needle)
+{
+    size_t pos = haystack.find(needle);
+    return (pos == wstring::npos) ? -1 : static_cast<int>(pos);
 }
